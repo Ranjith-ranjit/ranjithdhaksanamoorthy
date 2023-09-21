bbeeaa228289df6c477200d1b43a560007f5caf8
Ranjith-ranjit/Ranjith_ranjit_D81679F5EBEB8A798F64C39F184DBC86 <144785815+Ranjith-ranjit@users.noreply.github.com>
2023-09-21 11:40:31 +0530

    Update BankAccount.py

--- Challangeunit-2/BankAccount.py
+++ Challangeunit-2/BankAccount.py
@@ -28,7 +28,7 @@ class BankAccount:
 
 
 # Create an instance of the BankAccount class
-account = BankAccount(account_number="212201008", account_holder_name="Samsun Kiran", initial_balance=10000.0)
+account = BankAccount(account_number="212201002", account_holder_name="Ranjith", initial_balance=10000.0)
 
 # Test deposit and withdrawal functionality
 account.display_balance()
